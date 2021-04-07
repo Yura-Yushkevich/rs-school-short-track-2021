0a4c03089accb68d66e60da757a266a6cbbadb6a
Born <yura.yushkevich@gmail.com>
2021-04-07 15:01:10 +0300

    feat: create mine-sweeper

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,64 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(mat) {
+  const defaultMine = [];
+  for (let col = 0; col < mat.length; col++) {
+    const arrCreateArr = [];
+    for (let row = 0; row < mat[col].length; row++) {
+      if (mat[col][row] === true) {
+        arrCreateArr.push('!');
+      } else {
+        arrCreateArr.push(0);
+      }
+    }
+    defaultMine.push(arrCreateArr);
+  }
+
+  for (let col = 0; col < defaultMine.length; col++) {
+    for (let row = 0; row < defaultMine[col].length; row++) {
+      if (defaultMine[col][row] === '!') {
+        if (col !== 0) {
+          defaultMine[col - 1][row]++;
+          if (row < defaultMine[col].length - 1 && defaultMine[col - 1][row + 1] !== '!') {
+            defaultMine[col - 1][row + 1]++;
+          }
+          if (row !== 0 && defaultMine[col - 1][row - 1] !== '!') {
+            defaultMine[col - 1][row - 1]++;
+          }
+        }
+        if (row < defaultMine[col].length - 1) {
+          defaultMine[col][row + 1]++;
+        }
+        if (col < defaultMine.length - 1) {
+          defaultMine[col + 1][row]++;
+          if (row < defaultMine[col].length - 1 && defaultMine[col + 1][row + 1] !== '!') {
+            defaultMine[col + 1][row + 1]++;
+          }
+          if (row !== 0 && defaultMine[col + 1][row - 1] !== '!') {
+            defaultMine[col + 1][row - 1]++;
+          }
+        }
+        if (row !== 0) {
+          defaultMine[col][row - 1]++;
+        }
+      }
+    }
+  }
+
+  for (let col = 0; col < defaultMine.length; col++) {
+    for (let row = 0; row < defaultMine[col].length; row++) {
+      if (defaultMine[col][row] === '!') {
+        defaultMine[col][row] = 1;
+      }
+    }
+  }
+
+  return defaultMine;
 }
+
+// function minesweeper(/* matrix */) {
+//   throw new Error('Not implemented');
+// }
 
 module.exports = minesweeper;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,61 @@
-const arr = [-1, -1, -1, -1, -1];
+const matrix = [
+  [true, false, false],
+  [false, true, false],
+  [false, false, false]];
 
-function sortByHeight(ar) {
-  const arrPosOne = [];
-
-  ar.forEach((el, i) => {
-    if (el === -1) {
-      arrPosOne.push(i);
+function minesweeper(mat) {
+  const defaultMine = [];
+  for (let col = 0; col < mat.length; col++) {
+    const arrCreateArr = [];
+    for (let row = 0; row < mat[col].length; row++) {
+      if (mat[col][row] === true) {
+        arrCreateArr.push('!');
+      } else {
+        arrCreateArr.push(0);
+      }
     }
-  });
-
-  const arrNum = ar.filter((num) => num !== -1).sort((a, b) => a - b);
-
-  if (arrNum) {
-    arrPosOne.forEach((el) => {
-      arrNum.splice(el, 0, -1);
-    });
-  } else {
-    arrPosOne.forEach(() => {
-      arrNum.push(-1);
-    });
+    defaultMine.push(arrCreateArr);
   }
-  return arrNum;
-}
 
-console.log(sortByHeight(arr));
+  for (let col = 0; col < defaultMine.length; col++) {
+    for (let row = 0; row < defaultMine[col].length; row++) {
+      if (defaultMine[col][row] === '!') {
+        if (col !== 0) {
+          defaultMine[col - 1][row]++;
+          if (row < defaultMine[col].length - 1 && defaultMine[col - 1][row + 1] !== '!') {
+            defaultMine[col - 1][row + 1]++;
+          }
+          if (row !== 0 && defaultMine[col - 1][row - 1] !== '!') {
+            defaultMine[col - 1][row - 1]++;
+          }
+        }
+        if (row < defaultMine[col].length - 1) {
+          defaultMine[col][row + 1]++;
+        }
+        if (col < defaultMine.length - 1) {
+          defaultMine[col + 1][row]++;
+          if (row < defaultMine[col].length - 1 && defaultMine[col + 1][row + 1] !== '!') {
+            defaultMine[col + 1][row + 1]++;
+          }
+          if (row !== 0 && defaultMine[col + 1][row - 1] !== '!') {
+            defaultMine[col + 1][row - 1]++;
+          }
+        }
+        if (row !== 0) {
+          defaultMine[col][row - 1]++;
+        }
+      }
+    }
+  }
+
+  for (let col = 0; col < defaultMine.length; col++) {
+    for (let row = 0; row < defaultMine[col].length; row++) {
+      if (defaultMine[col][row] === '!') {
+        defaultMine[col][row] = 1;
+      }
+    }
+  }
+
+  return defaultMine;
+}
+console.log(minesweeper(matrix));
