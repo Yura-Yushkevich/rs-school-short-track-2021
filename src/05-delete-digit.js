ab164be5d69e636b5b4469a3fdf0180c9e41274a
Born <yura.yushkevich@gmail.com>
2021-04-07 16:41:48 +0300

    feat: create deleteDigit

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,24 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+
+function deleteDigit(n) {
+  const strNum = n.toString();
+  const arrNum = [];
+  let strLeft = '';
+  let strRight = '';
+  let strCommon = '';
+  for (let i = 0; i < strNum.length; i++) {
+    strLeft = strNum.slice(0, i);
+    strRight = strNum.slice(i + 1, strNum.length);
+    strCommon = strLeft + strRight;
+    arrNum.push(+strCommon);
+  }
+  return Math.max.apply(null, arrNum);
 }
+
+// function deleteDigit(/* n */) {
+//   throw new Error('Not implemented');
+// }
 
 module.exports = deleteDigit;
--- src/index.js
+++ src/index.js
@@ -1,61 +1,17 @@
-const matrix = [
-  [true, false, false],
-  [false, true, false],
-  [false, false, false]];
+const num = 1001;
 
-function minesweeper(mat) {
-  const defaultMine = [];
-  for (let col = 0; col < mat.length; col++) {
-    const arrCreateArr = [];
-    for (let row = 0; row < mat[col].length; row++) {
-      if (mat[col][row] === true) {
-        arrCreateArr.push('!');
-      } else {
-        arrCreateArr.push(0);
-      }
-    }
-    defaultMine.push(arrCreateArr);
+function deleteDigit(n) {
+  const strNum = n.toString();
+  const arrNum = [];
+  let strLeft = '';
+  let strRight = '';
+  let strCommon = '';
+  for (let i = 0; i < strNum.length; i++) {
+    strLeft = strNum.slice(0, i);
+    strRight = strNum.slice(i + 1, strNum.length);
+    strCommon = strLeft + strRight;
+    arrNum.push(+strCommon);
   }
-
-  for (let col = 0; col < defaultMine.length; col++) {
-    for (let row = 0; row < defaultMine[col].length; row++) {
-      if (defaultMine[col][row] === '!') {
-        if (col !== 0) {
-          defaultMine[col - 1][row]++;
-          if (row < defaultMine[col].length - 1 && defaultMine[col - 1][row + 1] !== '!') {
-            defaultMine[col - 1][row + 1]++;
-          }
-          if (row !== 0 && defaultMine[col - 1][row - 1] !== '!') {
-            defaultMine[col - 1][row - 1]++;
-          }
-        }
-        if (row < defaultMine[col].length - 1) {
-          defaultMine[col][row + 1]++;
-        }
-        if (col < defaultMine.length - 1) {
-          defaultMine[col + 1][row]++;
-          if (row < defaultMine[col].length - 1 && defaultMine[col + 1][row + 1] !== '!') {
-            defaultMine[col + 1][row + 1]++;
-          }
-          if (row !== 0 && defaultMine[col + 1][row - 1] !== '!') {
-            defaultMine[col + 1][row - 1]++;
-          }
-        }
-        if (row !== 0) {
-          defaultMine[col][row - 1]++;
-        }
-      }
-    }
-  }
-
-  for (let col = 0; col < defaultMine.length; col++) {
-    for (let row = 0; row < defaultMine[col].length; row++) {
-      if (defaultMine[col][row] === '!') {
-        defaultMine[col][row] = 1;
-      }
-    }
-  }
-
-  return defaultMine;
+  return Math.max.apply(null, arrNum);
 }
-console.log(minesweeper(matrix));
+console.log(deleteDigit(num));
