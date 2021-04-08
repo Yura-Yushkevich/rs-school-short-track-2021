0a2023732717cd35e2cd494285ebce3082dec39f
Born <yura.yushkevich@gmail.com>
2021-04-08 16:33:26 +0300

    feat: create find-index

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,24 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+
+function findIndex(array, value) {
+  let partFirst = 0;
+  let partSecond = array.length - 1;
+  let target = -1;
+  let devidePos = 0;
+
+  while (target === -1) {
+    devidePos = Math.floor((partFirst + partSecond) / 2);
+    if (array[devidePos] === value) {
+      target = devidePos;
+    } else if (array[devidePos] < value) {
+      partFirst = devidePos + 1;
+    } else if (array[devidePos] > value) {
+      partSecond = devidePos - 1;
+    }
+  }
+  return target;
 }
 
 module.exports = findIndex;
