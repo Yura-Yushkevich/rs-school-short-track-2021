2dcee6e7ed34c06a3198ba22078030da86c960c5
Born <yura.yushkevich@gmail.com>
2021-04-09 15:11:48 +0300

    feat: create get email

--- src/index.js
+++ src/index.js
@@ -1,52 +1,11 @@
-const ListNode = require('../extensions/list-node');
-
-class List {
-  constructor() {
-    this.head = null;
-    this.length = null;
-  }
-
-  get size() {
-    return this.length;
-  }
-
-  enqueue(element) {
-    const node = new ListNode(element);
-    console.log(node);
-    let currentNode = this.head;
-
-    if (!currentNode) {
-      this.head = node;
-      this.length++;
-      return node;
-    }
-
-    while (currentNode.next) {
-      currentNode = currentNode.next;
-    }
-    currentNode.next = node;
-    this.length++;
-    return node;
-  }
-
-  dequeue() {
-    let currentNode = this.head;
-    const nodeValue = currentNode.value;
-    currentNode = currentNode.next;
-    this.head = currentNode;
-    this.length--;
-    return nodeValue;
-  }
+function getEmailDomain(email) {
+  const strEmail = email;
+  const pos = strEmail.lastIndexOf('@');
+  return strEmail.slice(pos + 1);
 }
 
-const listTest = new List();
-listTest.enqueue(5);
-listTest.enqueue(7);
-
-// console.log(listTest.head);
-// console.log(listTest.length);
-
-listTest.dequeue();
-
-// console.log(listTest.head);
-// console.log(listTest.length);
+getEmailDomain('prettyandsimple@example.com');// , 'example.com');
+getEmailDomain('someaddress@yandex.ru');// , 'yandex.ru');
+getEmailDomain('very.unusual.@.unusual.com@usual.com');// , 'usual.com');
+getEmailDomain('admin@mailserver2.ru');// , 'mailserver2.ru');
+getEmailDomain('example-indeed@strange-example.com');// , 'strange-example.com');
